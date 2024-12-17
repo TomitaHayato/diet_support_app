6b12a0044d580f1e67248796f0719540c5dd95e4
hayato_52b <a99246780@icloud.com>
2024-12-18 06:40:07 +0900

    ADD: 新規登録フォームのUIを実装

--- frontend/src/components/general/SignupModal.jsx
+++ frontend/src/components/general/SignupModal.jsx
@@ -12,7 +12,35 @@ function SignupModal() {
             </form>
 
             {/* フォーム */}
-            <h2>ユーザー情報登録</h2>
+            <h2 className="text-center text-xl mb-3">アカウント新規作成</h2>
+            <form action="">
+              <label className="input input-bordered flex items-center gap-2 mb-8">
+                <i className="i-uiw-user"/>
+                <input type="text" className="grow" placeholder="もちもち太郎" />
+              </label>
+
+              <label className="input input-bordered flex items-center gap-2 mb-8">
+                <p className="text-gray-500">kg</p>
+                <input type="number" className="grow" placeholder="50" />
+              </label>
+
+              <label className="input input-bordered flex items-center gap-2 mb-8">
+                <i className="i-lucide-mail"/>
+                <input type="email" className="grow" placeholder="user@example.com" />
+              </label>
+
+              <label className="input input-bordered flex items-center gap-2 mb-8">
+                <i className="i-lucide-key-round"/>
+                <input type="password" className="grow" placeholder="Password" />
+              </label>
+
+              <label className="input input-bordered flex items-center gap-2 mb-8">
+                <i className="i-lucide-key-round"/>
+                <input type="password" className="grow" placeholder="Password Confirmation" />
+              </label>
+
+              <input type="submit" className="btn btn-accent btn-outline w-full" />
+            </form>
           </div>
 
           {/* 外部クリックで閉じる  */}
