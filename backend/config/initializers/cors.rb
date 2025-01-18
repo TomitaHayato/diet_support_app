7f74b37f79a88425c583e97093a3d06e9f472040
hayato_52b <a99246780@icloud.com>
2025-01-18 19:42:27 +0900

    Update: corsで許可するCloudfrontのURLを追加 + axiosでアクセスするURLをのproduction環境verを追加

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5173"
+    origins "localhost:5173", "https://web.calorie-work.online", "https://www.calorie-work.online", "https://calorie-work.online"
 
     resource "*",
       headers: :any,
