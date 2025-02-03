1719a9a72e5ec08573c874e08d9595b8244bfd0e
hayato_52b <a99246780@icloud.com>
2025-02-03 15:57:19 +0900

    ADD-TEST:front LoggedInContentsのテストを作成

--- frontend/src/tests/components/LoggedInContents.test.jsx
+++ frontend/src/tests/components/LoggedInContents.test.jsx
@@ -1,20 +1,22 @@
-import { beforeAll, describe, test } from "vitest";
+import { beforeAll, describe, expect, test, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import LoggedInContents from "../../components/general/sidemenu/LoggedInContents";
 
 describe('コンポーネント: LoggedInContentsのテスト', () => {
-
   beforeAll(() => {
     // 子コンポーネントのmock化
+    vi.mock('../../components/general/sidemenu/TodayData'           , () => ({ default: () => <div>TodayData</div>  }));
+    vi.mock('../../components/general/sidemenu/charts/DataCharts'   , () => ({ default: () => <div>DataCharts</div> }));
+    vi.mock('../../components/general/sidemenu/UserZone'            , () => ({ default: () => <div>UserZone</div>   }));
+    vi.mock('../../components/general/sidemenu/auth_form/LogoutForm', () => ({ default: () => <div>LogoutForm</div> }));
     // レンダリング
+    render(<LoggedInContents />)
   });
 
-  // 今日のデータ
-  test('今日のデータが表示される', () => {});
-  // 期間ごとのデータ
-  test('期間ごとのデータが表示される', () => {});
-  // プロフィール
-  test('プロフィールが表示される', () => {});
-  // プロフィール編集
-  test('プロフィール編集フォームを表示可能', () => {});
-  // ログアウト
-  test('ログアウトボタンが表示される', () => {});
+  test('コンテンツが表示される', () => {
+    expect(screen.getByText('TodayData'));
+    expect(screen.getByText('DataCharts'));
+    expect(screen.getByText('UserZone'));
+    expect(screen.getByText('LogoutForm'));
+  });
 });
