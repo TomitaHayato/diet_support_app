7e811a0b70f6474a465cee7af31c64f0108b036a
hayato_52b <a99246780@icloud.com>
2025-02-07 02:22:35 +0900

    Update:back seedで作成するタグ・Workoutを変更

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -12,24 +12,136 @@
 
 # Workoutのデータ作成
 workouts = [
-  { name: "散歩"                      , mets: 3.5  },
-  { name: "サイクリング(20km/h程度)"  , mets: 8.0  },
-  { name: "ラジオ体操第一"            , mets: 4.0  },
-  { name: "ラジオ体操第二"            , mets: 4.5  },
-  { name: "軽い筋トレ（腕立てや腹筋）", mets: 3.8  },
-  { name: "水泳"                      , mets: 5.3  },
-  { name: "ランニング"                , mets: 9.0  },
-  { name: "ジョギング"                , mets: 7.0  },
-  { name: "スクワット"                , mets: 5.0  },
-  { name: "ヨガ"                      , mets: 2.5  },
-  { name: "バドミントン"              , mets: 5.5  },
-  { name: "ピラティス"                , mets: 3.0  },
-  { name: "野球/ソフトボール"         , mets: 5.0  },
-  { name: "階段を登る（速く）"    , mets: 8.8  },
-  { name: "階段を上り下り（ゆっくり）"    , mets: 3.75 },
-  { name: "スポーツ/ダンスゲーム"     , mets: 3.8  },
-  { name: "犬の散歩"                  , mets: 3.0  },
-  { name: "電動自転車に乗る"          , mets: 3.0  }
+  # 「運動系」
+  # 低強度アクティビティ
+  { name: 'ストレッチング', mets: 2.3 },
+  { name: '全身を使ったテレビゲーム（バランス運動、ヨガ）', mets: 2.3 },
+  { name: 'ヨガ', mets: 2.5 },
+  { name: 'ビリヤード', mets: 2.5 },
+  { name: '座って行うラジオ体操', mets: 2.8 },
+  # 中強度アクティビティ
+  { name: 'ボウリング', mets: 3.0 },
+  { name: 'バレーボール', mets: 3.0 },
+  { name: '社交ダンス（ワルツ、サンバ、タンゴ）', mets: 3.0 },
+  { name: 'ピラティス', mets: 3.0 },
+  { name: '太極拳', mets: 3.0 },
+  { name: '自転車エルゴメーター(30～50ワット)', mets: 3.5 },
+  { name: '自体重を使った軽い筋力トレーニング（軽・中等度）', mets: 3.5 },
+  { name: '体操（家で、軽・中等度）', mets: 3.5 },
+  { name: 'ゴルフ（手引きカートを使って）', mets: 3.5 },
+  { name: 'カヌー', mets: 3.5 },
+  { name: '全身を使ったテレビゲーム（スポーツ・ダンス）', mets: 3.8 },
+  { name: '卓球', mets: 4.0 },
+  { name: 'パワーヨガ', mets: 4.0 },
+  { name: 'ラジオ体操第１', mets: 4.0 },
+  { name: 'やや速歩（平地、やや速めに=93m/分）', mets: 4.3 },
+  { name: 'ゴルフ（クラブを担いで運ぶ）', mets: 4.3 },
+  { name: 'テニス（ダブルス）＊', mets: 4.5 },
+  { name: '水中歩行（中等度）', mets: 4.5 },
+  { name: 'ラジオ体操第２', mets: 4.5 },
+  { name: '水泳（ゆっくりとした背泳）', mets: 4.8 },
+  { name: 'かなり速歩（平地、速く=107m/分）', mets: 5.0 },
+  { name: '野球', mets: 5.0 },
+  { name: 'ソフトボール', mets: 5.0 },
+  { name: 'サーフィン', mets: 5.0 },
+  { name: 'バレエ（モダン、ジャズ）', mets: 5.0 },
+  { name: '水泳（ゆっくりとした平泳ぎ）', mets: 5.3 },
+  { name: 'スキー', mets: 5.3 },
+  { name: 'アクアビクス', mets: 5.3 },
+  { name: 'バドミントン', mets: 5.5 },
+  # 高強度アクティビティ
+  { name: 'ゆっくりとしたジョギング', mets: 6.0 },
+  { name: 'ウェイトトレーニング（高強度、パワーリフティング、ボディビル）', mets: 6.0 },
+  { name: 'バスケットボール', mets: 6.0 },
+  { name: '水泳（のんびり泳ぐ）', mets: 6.0 },
+  { name: '山を登る(軽装)', mets: 6.5 },
+  { name: '自転車エルゴメーター（90～100ワット）', mets: 6.8 },
+  { name: 'ジョギング', mets: 7.0 },
+  { name: 'サッカー', mets: 7.0 },
+  { name: 'スキー', mets: 7.0 },
+  { name: 'スケート', mets: 7.0 },
+  { name: 'ハンドボール', mets: 7.0 },
+  { name: 'エアロビクス', mets: 7.3 },
+  { name: 'テニス（シングルス）', mets: 7.3 },
+  { name: '山を登る（約4.5～9.0kg の荷物を持って）', mets: 7.3 },
+  { name: 'サイクリング（約20km/時）', mets: 8.0 },
+  { name: 'ランニング（134m/分）', mets: 8.3 },
+  { name: '水泳（クロール、ふつうの速さ、46m/分未満）', mets: 8.3 },
+  { name: 'ラグビー', mets: 8.3 },
+  # 超高強度
+  { name: 'ランニング（139m/分）', mets: 9.0 },
+  { name: 'ランニング（161m/分）', mets: 9.8 },
+  { name: '水泳（クロール、速い、69m/分）', mets: 10.0 },
+  { name: '武道・武術（柔道、柔術、空手、キックボクシング、テコンドー）', mets: 10.3 },
+  { name: 'ランニング（188m/分）', mets: 11.0 },
+  { name: '自転車エルゴメーター（161～200ワット）', mets: 11.0 },
+
+  # 「生活運動系」
+  # 低強度アクティビティ
+  { name: '立位（会話、電話、読書)', mets: 1.8 },
+  { name: '皿洗い', mets: 1.8 },
+  { name: 'ゆっくりした歩行（非常に遅い）', mets: 2.0 },
+  { name: '料理や食材の準備', mets: 2.0 },
+  { name: '洗濯', mets: 2.0 },
+  { name: '子どもを抱えながら立つ', mets: 2.0 },
+  { name: '洗車・ワックスがけ', mets: 2.0 },
+  { name: '子どもと遊ぶ（座位、軽度）', mets: 2.2 },
+  { name: 'ガーデニング（コンテナを使用）', mets: 2.3 },
+  { name: '動物の世話', mets: 2.3 },
+  { name: 'ピアノの演奏', mets: 2.3 },
+  { name: '植物への水やり', mets: 2.5 },
+  { name: '子どもの世話', mets: 2.5 },
+  { name: '仕立て作業', mets: 2.5 },
+  { name: 'ゆっくりした歩行（遅い）', mets: 2.8 },
+  { name: '子ども・動物と遊ぶ（立位、軽度）', mets: 2.8 },
+  # // 中強度アクティビティ
+  { name: '普通歩行（67m/分、犬を連れて）', mets: 3.0 },
+  { name: '電動アシスト付き自転車', mets: 3.0 },
+  { name: '家財道具の片付け', mets: 3.0 },
+  { name: '子どもの世話（立位）', mets: 3.0 },
+  { name: '台所の手伝い', mets: 3.0 },
+  { name: '大工仕事', mets: 3.0 },
+  { name: '梱包', mets: 3.0 },
+  { name: 'ギター演奏（立位）', mets: 3.0 },
+  { name: 'カーペット掃き', mets: 3.3 },
+  { name: 'フロア掃き', mets: 3.3 },
+  { name: '掃除機', mets: 3.3 },
+  { name: 'スポーツ観戦（動きあり）', mets: 3.3 },
+  { name: '歩行（75～85m/分）', mets: 3.5 },
+  { name: '楽に自転車に乗る', mets: 3.5 },
+  { name: '階段を下りる', mets: 3.5 },
+  { name: '軽い荷物運び', mets: 3.5 },
+  { name: '車の荷物の積み下ろし', mets: 3.5 },
+  { name: '荷づくり', mets: 3.5 },
+  { name: 'モップがけ', mets: 3.5 },
+  { name: '床磨き', mets: 3.5 },
+  { name: '風呂掃除', mets: 3.5 },
+  { name: '庭の草むしり', mets: 3.5 },
+  { name: '子どもと遊ぶ（歩く/走る、中強度）', mets: 3.5 },
+  { name: '車椅子を押す', mets: 3.5 },
+  { name: '釣り（全般）', mets: 3.5 },
+  { name: 'スクーター/オートバイの運転', mets: 3.5 },
+  { name: '自転車に乗る（16km/時未満）', mets: 4.0 },
+  { name: '階段を上る（ゆっくり）', mets: 4.0 },
+  { name: '動物と遊ぶ（中強度）', mets: 4.0 },
+  { name: '高齢者の介護', mets: 4.0 },
+  { name: '屋根の雪下ろし', mets: 4.0 },
+  { name: 'やや速歩（93m/分）', mets: 4.3 },
+  { name: '苗木の植栽', mets: 4.3 },
+  { name: '農作業（家畜に餌を与える）', mets: 4.3 },
+  { name: '耕作', mets: 4.5 },
+  { name: '家の修繕', mets: 4.5 },
+  { name: 'かなり速歩（107m/分）', mets: 5.0 },
+  { name: '動物と遊ぶ（活発に）', mets: 5.0 },
+  { name: 'シャベルで土や泥をすくう', mets: 5.5 },
+  { name: '子どもと遊ぶ（活発に）', mets: 5.8 },
+  { name: '家具・家財道具の運搬', mets: 5.8 },
+  # // 高強度アクティビティ
+  { name: 'スコップで雪かきをする', mets: 6.0 },
+  { name: '農作業（干し草をまとめる）', mets: 7.8 },
+  { name: '重い荷物の運搬', mets: 8.0 },
+  { name: '荷物を上の階へ運ぶ', mets: 8.3 },
+  { name: '階段を上る（速く）', mets: 8.8 }
 ]
 
 workouts.each do |workout|
@@ -39,56 +151,44 @@ workouts.each do |workout|
 end
 
 # Tagのデータ作成
-# { name: "" },
 tags = [
-  { name: "イージー"   },
-  { name: "ハード"     },
-  { name: "家でできる" },
-  { name: 'アウトドア' },
-  { name: "ひとりで"   },
-  { name: "だれかと"   },
-  { name: "家事"       }
+  # 運動強度
+  { name: "低強度"       },
+  { name: "中強度"       },
+  { name: "高強度"       },
+  { name: "非常に高強度" },
+  # 場所
+  { name: "家でできる"   },
+  { name: "アウトドア"   },
+  # 人数
+  { name: "ひとりで"     },
+  { name: "だれかと"     },
+  # 種類
+  { name: "生活動作"     },
+  { name: "運動"     }
 ]
 
 tags.each do |tag|
   Tag.find_or_create_by!(name: tag[:name])
 end
 
-# WorkoutにTagを設定
-workout_tags = {
-  "散歩"                       => ["イージー", "アウトドア", "ひとりで"],
-  "サイクリング(20km/h程度)"   => ["アウトドア", "ひとりで"],
-  "ラジオ体操第一"             => ["イージー", "家でできる", "ひとりで"],
-  "ラジオ体操第二"             => ["イージー", "家でできる", "ひとりで"],
-  "軽い筋トレ（腕立てや腹筋）" => ["イージー", "家でできる", "ひとりで"],
-  "水泳"                       => ["ハード", "アウトドア", "ひとりで"],
-  "ランニング"                 => ["アウトドア", "ひとりで"],
-  "ジョギング"                 => ["イージー", "アウトドア", "ひとりで"],
-  "スクワット"                 => ["イージー", "家でできる", "ひとりで"],
-  "ヨガ"                       => ["イージー", "家でできる", "ひとりで"],
-  "バドミントン"               => ["ハード", "だれかと", "アウトドア"],
-  "ピラティス"                 => ["ひとりで", "家でできる"],
-  "野球/ソフトボール"          => ["ハード", "アウトドア", "だれかと"],
-  "階段を登る（速く）"         => ["ハード", "アウトドア", "ひとりで"],
-  "階段を上り下り（ゆっくり）" => ["アウトドア", "ひとりで"],
-  "スポーツ/ダンスゲーム"      => ["家でできる"],
-  "犬の散歩"                   => ["アウトドア", "ひとりで"],
-  "電動自転車に乗る"           => ["イージー", "アウトドア", "ひとりで"]
-}
+# Tagをworkoutに設定
+workouts = WorkOuts.all
 
-workout_hash = WorkOut.all.index_by(&:name)
-tag_hash     = Tag.all.index_by(&:name)
-
-WorkOut.transaction do
-  workout_tags.each do |workout_name, tag_arr|
-    workout = workout_hash[workout_name]
-    next if workout.nil?
-
-    tag_arr.each do |tag_name|
-      tag = tag_hash[tag_name]
-      next if tag.nil?
-
-      workout.tags << tag unless workout.tags.include?(tag)
-    end
+# 強度, 種類に関するTagを追加
+kind_of_work = '運動'
+workouts.each do |w|
+  if w.mets < 3
+    w.tags << Tag.find_by(name: '低強度')
+  elsif w.mets < 6
+    w.tags << Tag.find_by(name: '中強度')
+  elsif w.mets < 9
+    w.tags << Tag.find_by(name: '高強度')
+  else
+    w.tags << Tag.find_by(name: '非常に高強度')
   end
+
+  w.tags << Tag.find_by(name: kind_of_work)
+
+  kind_of_work = '生活動作' if w.name == "自転車エルゴメーター（161～200ワット）"
 end
