345ddfa62a99994a91a283f4d78c1aeefb403a8e
hayato_52b <a99246780@icloud.com>
2025-02-09 21:58:59 +0900

    ADD:Back apiにWorkoutのお気に入り登録・解除処理を追加

---
+++ backend/app/controllers/user_workout_likes_controller.rb
@@ -0,0 +1,30 @@
+class UserWorkoutLikesController < ApplicationController
+  before_action :authenticate_user! # 未認証ユーザーにstatus: :401を返す
+
+  # ログインユーザがお気に入りしている全Workoutを返す
+  def index
+    render json: current_user.workouts
+  end
+
+  def create
+    workout = Workout.find_by(id: params[:workout_id])
+
+    if workout
+      current_user.workouts << workout
+      render status: 200
+    else
+      render status: 422
+    end
+  end
+
+  def destroy
+    workout = Workout.find_by(id: params[:workout_id])
+
+    if workout
+      current_user.workouts.destroy!(workout)
+      render status: 200
+    else
+      render status: 422
+    end
+  end
+end
--- backend/app/models/user_workout_like.rb
+++ backend/app/models/user_workout_like.rb
@@ -2,7 +2,7 @@ class UserWorkoutLike < ApplicationRecord
   belongs_to :user
   belongs_to :workout
 
-  validates :user_id    , uniqueness: { scope: :workout_id }
-  validates :user_id    , presence: true
-  validates :work_out_id, presence: true 
+  validates :user_id   , uniqueness: { scope: :workout_id }
+  validates :user_id   , presence: true
+  validates :workout_id, presence: true
 end
