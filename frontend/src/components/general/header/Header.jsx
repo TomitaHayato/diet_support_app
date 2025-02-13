e5bbbc85f57a8cf0a36f5f93e0ae8a569630dd4e
hayato_52b <a99246780@icloud.com>
2025-02-13 22:40:23 +0900

    ADD:front ヘッダーにアイコンと参考を追加 + faviconを追加

--- frontend/src/components/general/header/Header.jsx
+++ frontend/src/components/general/header/Header.jsx
@@ -1,11 +1,25 @@
 function Header() {
   return (
     <>
-      <div data-testid="app-intro" className="text-center p-3 bg-base-200 border border-gray-500 rounded-lg">
-        <h3 className="mb-2 font-bold">カロリー管理をサポートします！！</h3>
+      <div data-testid="app-intro" className="grid grid-cols-4 p-3 bg-base-200 border border-gray-500 rounded-lg">
+        <span className="order-1">
+          <img src="/icon.png" className="max-w-20" />
+        </span>
 
-        <p className="text-sm">体重と消費したいカロリーを入力するだけで、必要な運動時間を簡単に計算できます！</p>
+        <div className="col-span-2 order-2 text-center">
+          <h3 className="mb-2 font-bold text-lg">カロリー管理をサポートします！！</h3>
+
+          <p className="text-sm">
+            体重と消費したいカロリーを入力するだけで<br />
+            必要な運動時間を簡単に計算できます！</p>
+        </div>
+
+        <span className="order-4"></span>
       </div>
+
+      <p className="text-xs pt-4 text-end">
+        参考： 厚生労働省「健康づくりのための身体活動・運動ガイド2023」
+      </p>
     </>
   )
 }
