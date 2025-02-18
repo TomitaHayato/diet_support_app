aee9ff09b76093fbe6032e1e5d8011c4eca31b59
hayato_52b <a99246780@icloud.com>
2025-02-18 17:51:36 +0900

    ADD:Front reduxのstoreを作成

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,17 @@ import './builds/build.css'
 import App from './pages/App.jsx';
 import { ThemeContextProvider } from './Contexts/ThemeProvider.jsx';
 import AuthsProvider from './Contexts/AuthsPorvider.jsx';
+import { Provider } from 'react-redux';
+import { store } from './Redux/Store/store.js';
 
 createRoot(document.getElementById('root')).render(
   <>
-    <ThemeContextProvider>
-      <AuthsProvider>
-        <App />
-      </AuthsProvider>
-    </ThemeContextProvider>
+    <Provider store={store}>
+      <ThemeContextProvider>
+        <AuthsProvider>
+          <App />
+        </AuthsProvider>
+      </ThemeContextProvider>
+    </Provider>
   </>,
 )
