7c0ea2f7c43abcd5dca3ff0c8a2af19ee021d092
hayato_52b <a99246780@icloud.com>
2025-02-19 18:55:42 +0900

    ADD:Front currentUserSliceを作成

---
+++ frontend/src/Redux/Slice/currentUserSlice.js
@@ -0,0 +1,142 @@
+/* eslint-disable no-unused-vars */
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getUser, logout, removeAuthToken, settingAuthTokenToCookie, signIn, signUp } from "../../utils/auth";
+
+// ログインユーザーを取得する処理
+export const fetchUserThunk = createAsyncThunk('currentUser/fetchUserThunk', async() => {
+  try {
+    // apiからデータを取得する処理
+    const res = await getUser();
+    // console.log(res.data);
+
+    if(!res) return;
+
+    const user = res.data.currentUser;
+    const likedWorkoutIds = res.data.likedWorkoutIds;
+
+    return { user, likedWorkoutIds }
+  } catch(e) {
+    console.log(e);
+  }
+})
+
+// ログイン処理
+export const loginThunk = createAsyncThunk('currentUser/loginThunk', async(params) => {
+  try {
+    // loginリクエスト
+    const res = await signIn(params);
+    if(!res) return;
+    // 認証TokenをCookieに保存
+    settingAuthTokenToCookie(res);
+    // ログインユーザーの情報を取得
+    const resUser = await getUser();
+    // console.log(res.data);
+
+    if(!resUser) return;
+
+    const user = resUser.data.currentUser;
+    const likedWorkoutIds = resUser.data.likedWorkoutIds;
+
+    return { user, likedWorkoutIds }
+  } catch(e) {
+    console.log(e)
+  }
+})
+
+// ログアウト処理
+export const logoutThunk = createAsyncThunk('currentUser/logoutThunk', async() => {
+  try {
+    // logoutリクエスト
+    await logout();
+    // CookieのToken削除
+    removeAuthToken();
+    return;
+  } catch(e) {
+    console.log(e);
+  }
+});
+
+// 新規登録処理
+export const signupThunk = createAsyncThunk('currentUser/signupThunk', async(params) => {
+  try {
+    const res = await signUp(params);
+    if(!res) return;
+    settingAuthTokenToCookie(res);
+    const user = res.data.data;
+    const likedWorkoutIds = []
+
+    return { user, likedWorkoutIds }
+  } catch(e) {
+    console.log(e);
+  }
+})
+
+const initialState = {
+  user: false,
+  likedWorkoutIds: false,
+  status: 'idle',
+  error: null,
+};
+
+const currentUserSlice = createSlice({
+  name: 'currentUser',
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      // fetchuserThunk
+      .addCase(fetchUserThunk.pending, (state, action) => {
+        state.status = 'pending';
+      })
+      .addCase(fetchUserThunk.fulfilled, (state, action) => {
+        state.status = 'successed';
+        state.user = action.payload.user;
+        state.likedWorkoutIds = action.payload.likedWorkoutIds;
+      })
+      .addCase(fetchUserThunk.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message ?? 'Unkown Error';
+      })
+      //LoginThunk
+      .addCase(loginThunk.pending, (state, action) => {
+        state.status = 'pending';
+      })
+      .addCase(loginThunk.fulfilled, (state, action) => {
+        state.status = 'successed';
+        state.user = action.payload.user;
+        state.likedWorkoutIds = action.payload.likedWorkoutIds;
+      })
+      .addCase(loginThunk.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message ?? 'Unkown Error';
+      })
+      // signupThunk
+      .addCase(signupThunk.pending, (state, action) => {
+        state.status = 'pending';
+      })
+      .addCase(signupThunk.fulfilled, (state, action) => {
+        state.status = 'successed';
+        state.user = action.payload.user;
+        state.likedWorkoutIds = action.payload.likedWorkoutIds;
+      })
+      .addCase(signupThunk.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message ?? 'Unkown Error';
+      })
+      // logoutThunk
+      .addCase(signupThunk.pending, (state, action) => {
+        state.status = 'pending';
+      })
+      .addCase(signupThunk.fulfilled, (state, action) => {
+        state.status = 'successed';
+        state.user = initialState.user;
+        state.likedWorkoutIds = initialState.likedWorkoutIds;
+      })
+      .addCase(signupThunk.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message ?? 'Unkown Error';
+      })
+  }
+});
+
+export const currentUserReducer = currentUserSlice.reducer;
--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { themeReducer } from "./Slice/ThemeSlice";
 import { intakedCalorieReducer } from "./Slice/intakedCalorieSlice";
 import { weightReducer } from "./Slice/WeightSlice";
+import { currentUserReducer } from "./Slice/currentUserSlice";
 
 export const store = configureStore({
   reducer: {
     theme:          themeReducer,
     intakedCalorie: intakedCalorieReducer,
     weight:         weightReducer,
+    currentUser:    currentUserReducer,
   }
 });
