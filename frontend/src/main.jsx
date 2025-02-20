ca94a52c7f870e25b75c92dcbe815e5fd04b3712
hayato_52b <a99246780@icloud.com>
2025-02-20 14:28:30 +0900

    Update:Front 不要なAuthscontextを削除

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './builds/build.css'
 import App from './pages/App.jsx';
-import AuthsProvider from './Contexts/AuthsPorvider.jsx';
 import { Provider } from 'react-redux';
 import { store } from './Redux/store.js';
 
 createRoot(document.getElementById('root')).render(
   <>
     <Provider store={store}>
-      <AuthsProvider>
-        <App />
-      </AuthsProvider>
+      <App />
     </Provider>
   </>,
 )
