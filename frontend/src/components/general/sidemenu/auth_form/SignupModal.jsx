b4f4126e63499902649fd81f2e51d3147fa4cc4a
hayato_52b <a99246780@icloud.com>
2025-03-04 17:48:46 +0900

    front:test SignupFormコンポーネントのUnitテスト

--- frontend/src/components/general/sidemenu/auth_form/SignupModal.jsx
+++ frontend/src/components/general/sidemenu/auth_form/SignupModal.jsx
@@ -1,9 +1,12 @@
 import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { signupThunk } from "../../../../Redux/Slice/currentUserSlice";
+import { useState } from "react";
+import { putDev } from "../../../../utils/devTool";
 
 function SignupModal() {
   const dispatch = useDispatch();
+  const [signupError, setSignupError] = useState(null);
 
   const { register, handleSubmit, formState: { errors } } = useForm();
 
@@ -11,7 +14,8 @@ function SignupModal() {
     try {
       dispatch(signupThunk(params));
     } catch(error) {
-      alert(error);
+      putDev(error);
+      setSignupError('新規登録に失敗しました');
     }
   }
 
@@ -19,23 +23,23 @@ function SignupModal() {
     <>
       <div>
         <p className="text-sm text-center mb-2">アカウント未登録の方はこちら</p>
-        <button className="btn btn-sm btn-outline btn-accent w-full" onClick={()=>document.getElementById('signup-form').showModal()}>
-          アカウント新規作成
+        <button className="btn btn-sm btn-outline btn-accent w-full" aria-label="signup-modal-open-button"
+          onClick={()=>document.getElementById('signup-form').showModal()}>アカウント新規作成
         </button>
 
-        <dialog id="signup-form" className="modal" aria-label="signup-form-modal">
-          <div className="modal-box">
+        <dialog id="signup-form" className="modal">
+          <div className="modal-box" role="modalbox" aria-label="signup-modalbox">
             {/* Close ボタン */}
             <form method="dialog">
-              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+              <button aria-label="modal-close-button" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
             </form>
 
             {/* フォーム */}
             <h2 className="text-center text-xl mb-3">アカウント新規作成</h2>
-            <form
-              onSubmit={handleSubmit(registerReq)}
-            >
-              {errors.name?.message && (<p className="text-red-500">{errors.name?.message}</p>)}
+            <form onSubmit={handleSubmit(registerReq)}>
+              <p className="text-red-500 text-lg" role="error-message" aria-label="signup-error">{signupError}</p>
+
+              {errors.name?.message && (<p className="text-red-500" role="error" aria-label="signup-name-error">{errors.name?.message}</p>)}
               <label className="input input-bordered flex items-center gap-2 mb-8">
                 <i className="i-uiw-user"/>
                 <input type="text" className="grow" placeholder="もちもち太郎"
@@ -44,7 +48,7 @@ function SignupModal() {
                 />
               </label>
 
-              {errors.email?.message && (<p className="text-red-500">{errors.email?.message}</p>)}
+              {errors.email?.message && (<p className="text-red-500" role="error" aria-label="signup-email-error">{errors.email?.message}</p>)}
               <label className="input input-bordered flex items-center gap-2 mb-8">
                 <i className="i-lucide-mail"/>
                 <input type="email" className="grow" placeholder="user@example.com"
@@ -53,21 +57,21 @@ function SignupModal() {
                 />
               </label>
 
-              {errors.password?.message && (<p className="text-red-500">{errors.password?.message}</p>)}
+              {errors.password?.message && (<p className="text-red-500" role="error" aria-label="signup-password-error">{errors.password?.message}</p>)}
               <label className="input input-bordered flex items-center gap-2 mb-8">
                 <i className="i-lucide-key-round"/>
                 <input type="password" className="grow" placeholder="Password"
                   aria-label="signup-password" role="passwordbox"
                   {...register("password", {
-                      required:  '*パスワードを入力してください。',
-                      minLength: {
-                        value:    6,
-                        message: '6文字以上必要です',
-                      }})}
+                    required:  '*パスワードを入力してください。',
+                    minLength: {
+                      value:    6,
+                      message: '6文字以上必要です',
+                  }})}
                 />
               </label>
 
-              {errors.passwordConfirmation?.message && (<p className="text-red-500">{errors.passwordConfirmation.message}</p>)}
+              {errors.passwordConfirmation?.message && (<p className="text-red-500" role="error" aria-label="signup-password-confirmation-error">{errors.passwordConfirmation.message}</p>)}
               <label className="input input-bordered flex items-center gap-2 mb-8">
                 <i className="i-lucide-key-round"/>
                 <input type="password" className="grow" placeholder="Password Confirmation"
@@ -81,13 +85,13 @@ function SignupModal() {
                 />
               </label>
 
-              <input type="submit" className="btn btn-accent btn-outline w-full"/>
+              <input type="submit" aria-label="signup-submit" className="btn btn-accent btn-outline w-full"/>
             </form>
           </div>
 
           {/* 外部クリックで閉じる  */}
           <form method="dialog" className="modal-backdrop">
-            <button>close</button>
+            <button></button>
           </form>
         </dialog>
       </div>
