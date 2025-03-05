fd034bd7b03305274b4d127d7a48fe251cebd3b1
hayato_52b <a99246780@icloud.com>
2025-03-05 23:49:23 +0900

    Front:Test LoggedinContentsのテストを作成

--- frontend/src/tests/components/LoggedInContents.test.jsx
+++ frontend/src/tests/components/LoggedInContents.test.jsx
@@ -1,22 +1,42 @@
-import { beforeAll, describe, expect, test, vi } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { afterEach, beforeEach, describe, expect, test, vi } from "vitest";
+import { cleanup, screen } from "@testing-library/react";
 import LoggedInContents from "../../components/general/sidemenu/LoggedInContents";
+import { renderWithProviders } from "../testUtils";
 
 describe('コンポーネント: LoggedInContentsのテスト', () => {
-  beforeAll(() => {
+  beforeEach(() => {
     // 子コンポーネントのmock化
-    vi.mock('../../components/general/sidemenu/TodayData'           , () => ({ default: () => <div>TodayData</div>  }));
-    vi.mock('../../components/general/sidemenu/charts/DataCharts'   , () => ({ default: () => <div>DataCharts</div> }));
-    vi.mock('../../components/general/sidemenu/UserZone'            , () => ({ default: () => <div>UserZone</div>   }));
-    vi.mock('../../components/general/sidemenu/auth_form/LogoutForm', () => ({ default: () => <div>LogoutForm</div> }));
-    // レンダリング
-    render(<LoggedInContents />)
+    vi.mock('../../components/general/sidemenu/TodayData', () => {
+      return {
+        default: vi.fn().mockImplementation(() => <p>TodayData</p>),
+      }
+    });
+    vi.mock('../../components/general/sidemenu/charts/DataCharts', () => {
+      return {
+        default: vi.fn().mockImplementation(() => <p>DataCharts</p>),
+      }
+    });
+    vi.mock('../../components/general/sidemenu/UserZone', () => {
+      return {
+        default: vi.fn().mockImplementation(() => <p>UserZone</p>),
+      }
+    });
+    vi.mock('../../components/general/sidemenu/auth_form/LogoutForm', () => {
+      return {
+        default: vi.fn().mockImplementation(() => <p>LogoutForm</p>),
+      }
+    });
   });
 
+  afterEach(() => {
+    cleanup();
+  })
+
   test('コンテンツが表示される', () => {
-    expect(screen.getByText('TodayData'));
-    expect(screen.getByText('DataCharts'));
-    expect(screen.getByText('UserZone'));
-    expect(screen.getByText('LogoutForm'));
+    renderWithProviders(<LoggedInContents />);
+    expect(screen.getByText('TodayData')).toBeInTheDocument();
+    expect(screen.getByText('DataCharts')).toBeInTheDocument();
+    expect(screen.getByText('UserZone')).toBeInTheDocument();
+    expect(screen.getByText('LogoutForm')).toBeInTheDocument();
   });
 });
