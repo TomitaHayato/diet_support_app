5c6ac30e3fde5005b83ff89bf7ada1ad9bff6f30
hayato_52b <a99246780@icloud.com>
2025-03-06 15:19:16 +0900

    Front:Test UserZoneのテストを作成

--- frontend/src/components/general/sidemenu/UserEditForm.jsx
+++ frontend/src/components/general/sidemenu/UserEditForm.jsx
@@ -31,7 +31,7 @@ function UserEditForm(props) {
       <div className="text-center">
         <div className="flex justify-center gap-3 mb-2">
           <p className="text-lg">プロフィール編集</p>
-          <button onClick={() => setEditMode(false)}>
+          <button aria-label="info-mode-btn" onClick={() => setEditMode(false)}>
             <i className="i-uiw-close hover:scale-110 hover:bg-red-500 active:scale-95" />
           </button>
         </div>
@@ -42,26 +42,26 @@ function UserEditForm(props) {
 
           {errors.name?.message && (<p className="text-red-500">{errors.name.message}</p>)}
           <label className="input input-sm input-bordered flex items-center gap-2 mb-2">
-            名前:
+            名前
             <input type="text" className="grow" defaultValue={currentUser.name}
               {...register('name', {required: '*名前を入力してください'})}/>
           </label>
 
           {errors.weight?.message && (<p className="text-red-500">{errors.weight.message}</p>)}
           <label className="input input-sm input-bordered flex items-center gap-2 mb-2">
-            体重:
+            体重
             <input type="integer" className="grow" defaultValue={currentUser.weight}
               {...register('weight', {required: '*体重を入力してください', min: {value: 0, message: '*0以上の整数を入力してください'}})}/>
           </label>
 
           {errors.email?.message && (<p className="text-red-500">{errors.email.message}</p>)}
           <label className="input input-sm input-bordered flex items-center gap-2 mb-2">
-            メール:
+            メール
             <input type="email" className="grow" defaultValue={currentUser.email}
               {...register('email', {required: 'メールアドレスを入力してください'})}/>
           </label>
 
-          <button className="btn btn-sm btn-info btn-outline w-full mb-2">
+          <button aria-label="user-update-btn" className="btn btn-sm btn-info btn-outline w-full mb-2">
             更新する
           </button>
         </form>
--- frontend/src/tests/components/UserZone.test.jsx
+++ frontend/src/tests/components/UserZone.test.jsx
@@ -1,7 +1,38 @@
-import { describe, test } from "vitest";
+import { cleanup, screen } from "@testing-library/react";
+import { afterEach, beforeEach, describe, expect, test } from "vitest";
+import { dammyCurrentUser, renderWithProviders } from "../testUtils";
+import UserZone from "../../components/general/sidemenu/UserZone";
+import userEvent from "@testing-library/user-event";
 
 describe('コンポーネント： UserZone.jsxのテスト', () => {
-  test('初期表示', () => {});
+  beforeEach(() => {});
 
-  test('editModeステートの値でUserInfo, UserEditFormを切り替え表示', () => {});
+  afterEach(() => cleanup());
+
+  test('初期表示: Userinfoがレンダリングされ、ログインユーザーの情報が表示される', async() => {
+    const user = userEvent.setup();
+    renderWithProviders(<UserZone />, {
+      preloadedState: {
+        currentUser: dammyCurrentUser(),
+      }
+    });
+    const currentUser = dammyCurrentUser().user;
+    expect(screen.getByRole('button'   , {name: 'edit-mode-btn'})).toBeInTheDocument();
+    expect(screen.getByRole('user-info', {name: 'name'})         ).toHaveTextContent(currentUser.name);
+    expect(screen.getByRole('user-info', {name: 'weight'})       ).toHaveTextContent(currentUser.weight);
+    expect(screen.getByRole('user-info', {name: 'email'})        ).toBeInTheDocument();
+    expect(screen.queryByRole('button' , {name: 'info-mode-btn'})).not.toBeInTheDocument();
+    // UserEditFormに切り替え表示できる
+    await user.click(screen.getByRole('button', {name: 'edit-mode-btn'}));
+    expect(screen.getByRole('button'  , {name: 'info-mode-btn'})  ).toBeInTheDocument();
+    expect(screen.getByRole('button'  , {name: 'user-update-btn'})).toBeInTheDocument();
+    expect(screen.queryByRole('button', {name: 'edit-mode-btn'})  ).not.toBeInTheDocument();
+    expect(screen.getByLabelText('名前')  ).toBeInTheDocument();
+    expect(screen.getByLabelText('体重')  ).toBeInTheDocument();
+    expect(screen.getByLabelText('メール')).toBeInTheDocument();
+    // userInfoに戻せる
+    await user.click(screen.getByRole('button', {name: 'info-mode-btn'}));
+    expect(screen.getByRole('button'  , {name: 'edit-mode-btn'})  ).toBeInTheDocument();
+    expect(screen.queryByRole('button', {name: 'info-mode-btn'})  ).not.toBeInTheDocument();
+  });
 });
