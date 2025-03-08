5a6cb2bdc8bd4fd5641800357289820cd61b768d
hayato_52b <a99246780@icloud.com>
2025-03-09 02:35:07 +0900

    UpdateFront: DataChartsコンポーネントの責務をより明確化

--- frontend/src/components/general/sidemenu/charts/DataCharts.jsx
+++ frontend/src/components/general/sidemenu/charts/DataCharts.jsx
@@ -5,34 +5,23 @@ import BarChartMonth from "./BarChartMonth";
 function DataCharts() {
   return (
     <>
-      <div className="text-center">
-        <div className="collapse collapse-arrow">
-          <input type="checkbox" />
-          <h3 className="collapse-title text-lg font-medium flex items-center justify-center">
-            運動記録<i className="i-lucide-bar-chart-4 text-blue-600 text-xl ml-2"/>
-          </h3>
+      <div role="tablist" className="tabs tabs-bordered grid-cols-3">
+        {/* 今週のチャート */}
+        <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="週間" defaultChecked />
+        <div role="tabpanel" className="tab-content pt-5">
+          <BarChartWeek />
+        </div>
 
-          <div className="collapse-content px-0">
-            <div role="tablist" className="tabs tabs-bordered grid-cols-3">
-              {/* 今週のチャート */}
-              <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="週間" defaultChecked />
-              <div role="tabpanel" className="tab-content pt-5">
-                <BarChartWeek />
-              </div>
+        {/* 今月のチャート */}
+        <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="月間"/>
+        <div role="tabpanel" className="tab-content pt-5">
+          <BarChartMonth />
+        </div>
 
-              {/* 今月のチャート */}
-              <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="月間"/>
-              <div role="tabpanel" className="tab-content pt-5">
-                <BarChartMonth />
-              </div>
-
-              {/* 年ごとのチャート */}
-              <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="年間" />
-              <div role="tabpanel" className="tab-content pt-5">
-                <BarChartYear />
-              </div>
-            </div>
-          </div>
+        {/* 年ごとのチャート */}
+        <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="年間" />
+        <div role="tabpanel" className="tab-content pt-5">
+          <BarChartYear />
         </div>
       </div>
     </>
