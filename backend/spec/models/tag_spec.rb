93bd9bcdad438ea36be7774a6a4ac2a3d60d5b6e
hayato_52b <a99246780@icloud.com>
2025-03-12 02:25:32 +0900

    back:test:fix tag_specでtag変数が定義されていないバグを修正

--- backend/spec/models/tag_spec.rb
+++ backend/spec/models/tag_spec.rb
@@ -3,9 +3,7 @@ require 'rails_helper'
 RSpec.describe Tag, type: :model do
   describe 'association' do
     describe 'workout, workout_tagsとの関連' do
-      before do
-        tag = create(:tag, :with_workout)
-      end
+      let!(:tag) { create(:tag, :with_workout) }
 
       it 'tag.workout_tags' do
         expect(tag.workout_tags.length).to eq 3
