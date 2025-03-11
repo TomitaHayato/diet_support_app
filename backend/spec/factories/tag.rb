5e8db3b1cfd4b30eaf8c5de61c98c5315fb8c136
hayato_52b <a99246780@icloud.com>
2025-03-11 15:42:58 +0900

    back:test tag_specを作成

--- backend/spec/factories/tag.rb
+++ backend/spec/factories/tag.rb
@@ -1,6 +1,16 @@
 FactoryBot.define do
   factory :tag do
     sequence(:name) {|n| "tag#{n}"}
+    
+    trait :no_valid do
+      name { nil }
+    end
+
+    trait :with_workout do
+      after(:create) do |tag|
+        create_list(:workout_tag, 3, tag:)
+      end
+    end
   end
 end
 
--- backend/spec/models/tag_spec.rb
+++ backend/spec/models/tag_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'association' do
+    describe 'workout, workout_tagsとの関連' do
+      before do
+        tag = create(:tag, :with_workout)
+      end
+
+      it 'tag.workout_tags' do
+        expect(tag.workout_tags.length).to eq 3
+      end
+
+      it 'tag.workouts' do
+        expect(tag.workouts).to eq 3
+      end
+    end
+  end
+
+  describe 'validation' do
+    it 'nameカラムが必須' do
+      tag = build(:tag, :no_valid)
+      expect(tag).to be_invalid
+    end
+  end
 end
