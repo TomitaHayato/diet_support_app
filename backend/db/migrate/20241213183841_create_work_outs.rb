835101fe0c06195af89ec6f5e5b0d1561e6b3204
hayato_52b <a99246780@icloud.com>
2025-03-11 23:16:41 +0900

    ci/cd　エラー修正9: mysql起動コマンドの削除＋マイグレーションファイルの誤字修正

--- backend/db/migrate/20241213183841_create_work_outs.rb
+++ backend/db/migrate/20241213183841_create_work_outs.rb
@@ -1,4 +1,4 @@
-class CreateWorkouts < ActiveRecord::Migration[7.2]
+class CreateWorkOuts < ActiveRecord::Migration[7.2]
   def change
     create_table :work_outs do |t|
       t.string :name   , null: false
