59dbc80393501443a49921095e81512f3e69163e
hayato_52b <a99246780@icloud.com>
2025-03-12 14:02:07 +0900

    back:test shoulder-matcherを使用して、workout_specのassociationテストを記述

--- backend/spec/models/workout_spec.rb
+++ backend/spec/models/workout_spec.rb
@@ -3,26 +3,38 @@ require 'rails_helper'
 RSpec.describe Workout, type: :model do
   describe 'association' do
     describe 'Workout:Tag = M:N'
-      it 'has_many :workout_tags, dependent: :destroy' do
-      end
+      it { should have_many(:workout_tags) }
 
-      it 'has_many :tags, through: :workout_tags' do
+      it { should have_many(:tags) }
+
+      it 'workoutが削除 => workout_tagも削除(dependent: :destroy)' do
+        workout = create(:workout)
+        create(:workout_tag, workout:)
+        expect { workout.destroy! }.to change { WorkoutTag.count }.by(-1)
       end
     end
 
     describe 'Workout:User = M:N, join: user_workout_like' do
-      it 'has_many :user_workout_likes, dependent: :destroy' do
-      end
+      it { should have_many(:user_workout_likes) }
 
-      it 'has_many :users, through: :user_workout_likes' do
+      it { should have_many(:users) }
+
+      it 'workoutが削除 => user_workout_likesも削除(dependent::destroy)' do
+        workout = create(:workout)
+        create(:user_workout_like, workout:)
+        expect { workout.destroy! }.to change { UserWorkoutLike.count }.by(-1)
       end
     end
 
     describe 'Workout:User = M:N, join: workout_record' do
-      it 'has_many :workout_records, dependent: :nullify' do
-      end
+      it { should have_many(:workout_records) }
 
-      it 'has_many :record_users   , through: :workout_records, source: :user' do
+      it { should have_many(:record_users) }
+
+      it 'workoutが削除 => workout_recordsのworkout_idがnullになる(dependent: :nullify)' do
+        workout = create(:workout)
+        create(:workout_record, workout:)
+        expect { workout.destroy! }.to change { WorkoutRecord.last.workout_id }.from(workout.id).to(nil)
       end
     end
   end
