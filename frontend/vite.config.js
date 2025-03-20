10409c90d73ff4c3d76e3847877caf95185b75db
hayato_52b <a99246780@icloud.com>
2025-03-20 17:03:16 +0900

    ADD:Front vite-plugin-pwaドキュメントのPWAの最小要件の手順を追記

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,34 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Calorie Work',
+        short_name: 'Calorie Work',
+        description: '消費したいカロリーを入力すると、文部科学省のデータに基づいて必要な運動時間を自動計算できるWebサービスです。',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
+  ],
   test: {
     environment: 'happy-dom',
     root: './src/',
