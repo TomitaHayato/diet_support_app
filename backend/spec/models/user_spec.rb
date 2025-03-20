960cd50b144b79a05aaae8df3b524f6521979367
hayato_52b <a99246780@icloud.com>
2025-03-20 17:56:22 +0900

    FIX:test user_specの誤字を修正

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -28,8 +28,7 @@ RSpec.describe User, type: :model do
       context '今日のRecordがある場合' do
         it '今日のrecordだけを返す' do
           result = user.get_today_record
-          expected_result = 
-            user.workout_records.today_data
+          expected_result = user.workout_records.today_data
           expect(result.total_time).to              eq(expected_result.total_time)
           expect(result.total_burned_calories).to   eq(expected_result.total_burned_calories)
           expect(result.total_intaked_calories).to  eq(expected_result.total_intaked_calories)
