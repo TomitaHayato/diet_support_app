d7edd32aeaea12d8494a0e868b1389fac60b1312
hayato_52b <a99246780@icloud.com>
2025-03-21 17:01:17 +0900

    ADD:Front 指定期間のデータを取得するリクエスト送信処理及びThunkを追加

--- frontend/src/Redux/Slice/workoutRecordsSlice.js
+++ frontend/src/Redux/Slice/workoutRecordsSlice.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import { putDev } from "../../utils/devTool";
-import { getWorkoutRecords, postWorkoutRecord } from "../../utils/workoutRecordRequest";
+import { getMonthlyData, getWorkoutRecords, getYearlyData, postWorkoutRecord } from "../../utils/workoutRecordRequest";
 
 // レコードデータをapiから取得
 export const getWorkoutRecordsThunk = createAsyncThunk(
@@ -22,6 +22,7 @@ export const getWorkoutRecordsThunk = createAsyncThunk(
       return { todayData, weeklyData, monthlyData, yearlyData, historyData };
     } catch(e) {
       putDev(e);
+      return rejectWithValue('Error: データの取得に失敗しました。');
   }},
   {
     // pending状態の場合、重複して処理を行わない
@@ -34,6 +35,87 @@ export const getWorkoutRecordsThunk = createAsyncThunk(
     }
   }
 );
+
+// 指定した年のレコードを取得する
+export const getYearlyDataThunk = createAsyncThunk('workoutRecords/getYearlyDataThunk',
+  async(yearsAgo, {rejectWithValue}) => {
+    try{
+      const res = await getYearlyData(yearsAgo);
+      putDev("getYearlyDataのres")
+      putDev(res);
+      if(!res.ok) rejectWithValue('Error: データの取得に失敗しました。');
+      return res.data;
+    } catch(e) {
+      putDev('getYearlyDataThunkのエラー');
+      putDev(e);
+      return rejectWithValue('Error: データの取得に失敗しました。');
+    }
+  },
+  {
+    // pending状態の場合、重複して処理を行わない
+    condition(arg, thunkApi) {
+      const status = selectWorkoutRecordsStatus(thunkApi.getState());
+      if (status === 'pending') {
+        putDev('重複リクエストをキャンセルします');
+        return false;
+      }
+    }
+  }
+)
+
+// 指定した月のレコードを取得する
+export const getMonthlyDataThunk = createAsyncThunk('workoutRecords/getMonthlyDataThunk',
+  async(monthAgo, {rejectWithValue}) => {
+    try{
+      const res = await getMonthlyData(monthAgo);
+      putDev("getMonthlyDataのres")
+      putDev(res);
+      if(!res.ok) rejectWithValue('Error: データの取得に失敗しました。');
+      return res.data;
+    } catch(e) {
+      putDev('getMonthlyDataThunkのエラー');
+      putDev(e);
+      return rejectWithValue('Error: データの取得に失敗しました。');
+    }
+  },
+  {
+    // pending状態の場合、重複して処理を行わない
+    condition(arg, thunkApi) {
+      const status = selectWorkoutRecordsStatus(thunkApi.getState());
+      if (status === 'pending') {
+        putDev('重複リクエストをキャンセルします');
+        return false;
+      }
+    }
+  }
+)
+
+// 指定した週のレコードを取得する
+export const getWeeklyDataThunk = createAsyncThunk('workoutRecords/getWeeklyDataThunk',
+  async(weeksAgo, {rejectWithValue}) => {
+    try{
+      const res = await getMonthlyData(weeksAgo);
+      putDev("getWeeklyDataのres")
+      putDev(res);
+      if(!res.ok) rejectWithValue('Error: データの取得に失敗しました。');
+      return res.data;
+    } catch(e) {
+      putDev('getWeeklyDataThunkのエラー');
+      putDev(e);
+      return rejectWithValue('Error: データの取得に失敗しました。');
+    }
+  },
+  {
+    // pending状態の場合、重複して処理を行わない
+    condition(arg, thunkApi) {
+      const status = selectWorkoutRecordsStatus(thunkApi.getState());
+      if (status === 'pending') {
+        putDev('重複リクエストをキャンセルします');
+        return false;
+      }
+    }
+  }
+)
 
 export const createWorkoutRecordThunk = createAsyncThunk(
   'workoutRecords/createWorkoutRecordThunk',
@@ -52,6 +134,7 @@ export const createWorkoutRecordThunk = createAsyncThunk(
       return { todayData, weeklyData, monthlyData, yearlyData, historyData };
     } catch(e) {
       putDev(e);
+      return rejectWithValue('Error: データの取得に失敗しました');
     }
   },
   {
@@ -95,12 +178,49 @@ const workoutRecordsSlice = createSlice({
         state.monthlyData = action.payload.monthlyData;
         state.yearlyData  = action.payload.yearlyData;
         state.historyData = action.payload.historyData;
-        putDev('fulfilledのhistory')
-        putDev(action.payload.historyData)
       })
       .addCase(getWorkoutRecordsThunk.rejected, (state, action) => {
         state.status = 'failed';
-        state.error = action.error.message ?? 'Unkown Error';
+        state.error = action.error.message || 'Unkown Error';
+      })
+      // getYearlyDataThunk
+      .addCase(getYearlyDataThunk.pending, (state, _action) => {
+        state.status = 'pending';
+      })
+      .addCase(getYearlyDataThunk.fulfilled, (state, action) => {
+        state.status = 'successed';
+        if(!action.payload) return;
+        state.yearlyData = action.payload;
+      })
+      .addCase(getYearlyDataThunk.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message || 'Unkown Error';
+      })
+      // getMonthlyDataThunk
+      .addCase(getMonthlyDataThunk.pending, (state, _action) => {
+        state.status = 'pending';
+      })
+      .addCase(getMonthlyDataThunk.fulfilled, (state, action) => {
+        state.status = 'successed';
+        if(!action.payload) return;
+        state.monthlyData = action.payload;
+      })
+      .addCase(getMonthlyDataThunk.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message || 'Unkown Error';
+      })
+      // getYearlyDataThunk
+      .addCase(getWeeklyDataThunk.pending, (state, _action) => {
+        state.status = 'pending';
+      })
+      .addCase(getWeeklyDataThunk.fulfilled, (state, action) => {
+        state.status = 'successed';
+        if(!action.payload) return;
+        state.weeklyData = action.payload;
+      })
+      .addCase(getWeeklyDataThunk.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message || 'Unkown Error';
       })
       // createWorkoutRecordThunk
       .addCase(createWorkoutRecordThunk.pending, (state, _action) => {
@@ -118,7 +238,7 @@ const workoutRecordsSlice = createSlice({
       })
       .addCase(createWorkoutRecordThunk.rejected, (state, action) => {
         state.status = 'failed';
-        state.error = action.error.message ?? 'Unkown Error';
+        state.error = action.error.message || 'Unkown Error';
       })
   },
 });
--- frontend/src/utils/workoutRecordRequest.js
+++ frontend/src/utils/workoutRecordRequest.js
@@ -19,3 +19,36 @@ export function getWorkoutRecords() {
     headers: authTokensInCookie()
   })
 }
+
+// 指定した期間のyearlyデータを取得
+export function getYearlyData(yearsAgo) {
+  //tokenがない場合は何もしない
+  if(!isAccessTokenInCookie()) return;
+
+  return client.get('/workout_records_yearly', {
+    headers: authTokensInCookie(),
+    params: { yearsAgo },
+  });
+}
+
+// 指定した期間のmonthlyデータを取得
+export function getMonthlyData(monthAgo) {
+  //tokenがない場合は何もしない
+  if(!isAccessTokenInCookie()) return;
+
+  return client.get('/workout_records_monthly', {
+    headers: authTokensInCookie(),
+    params: { monthAgo },
+  });
+}
+
+// 指定した期間のweeklyデータを取得
+export function getWeeklyData(weeksAgo) {
+  //tokenがない場合は何もしない
+  if(!isAccessTokenInCookie()) return;
+
+  return client.get('/workout_records_weekly', {
+    headers: authTokensInCookie(),
+    params: { weeksAgo },
+  });
+}
