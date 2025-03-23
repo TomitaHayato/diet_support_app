dd1a378d333f0437d6ebb4c654eb267c2436c4f1
hayato_52b <a99246780@icloud.com>
2025-03-24 05:23:29 +0900

    ADD:Back applicationコントローラにprotect_from_forgeryを追加

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
   # devise_token_authから提供されるconcernをインポート
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  protect_from_forgery with: :exception
 end
