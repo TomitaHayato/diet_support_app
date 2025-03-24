f4b395fb28a7bf4f705f4bd19c45fb985a1f3a2b
hayato_52b <a99246780@icloud.com>
2025-03-24 22:28:35 +0900

    ADD:Back RailsにCSRFトークンの検証を行うための設定を追加

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   # devise_token_authから提供されるconcernをインポート
   include DeviseTokenAuth::Concerns::SetUserByToken
+  # CSRF Token検証に必要
+  # include ActionController::RequestForgeryProtection
 
   protect_from_forgery with: :exception
 end
--- backend/config/initializers/omniauth.rb
+++ backend/config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer if Rails.env.development?
   provider :google_oauth2 , Rails.application.credentials.google[:client_id], Rails.application.credentials.google[:client_secret]
+  OmniAuth.config.allowed_request_methods = %i[post get]
 end
