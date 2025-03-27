c32f8a6b05723b9648ec00a4f8c873cd480965a7
hayato_52b <a99246780@icloud.com>
2025-03-27 22:36:50 +0900

    ADD:Back omniauthのdeveloperストラテジとgoogle-oauth2ストラテジに対応できるように設定を追加

--- backend/config/initializers/omniauth.rb
+++ backend/config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
+OmniAuth.config.allowed_request_methods = [:post, :get]
+OmniAuth.config.silence_get_warning = true
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer if Rails.env.development?
+  provider :developer unless Rails.env.production?
   provider :google_oauth2 , Rails.application.credentials.google[:client_id], Rails.application.credentials.google[:client_secret]
-  OmniAuth.config.allowed_request_methods = %i[post get]
 end
